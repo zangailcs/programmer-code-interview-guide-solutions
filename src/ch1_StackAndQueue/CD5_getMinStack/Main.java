1b9a1471918ac4af396f1f87234690d8242ada01
wzh-111 <lics@seu.edu.cn>
2021-05-21 12:13:23 +0800

    min stack

--- src/ch1_StackAndQueue/CD5_getMinStack/Main.java
+++ src/ch1_StackAndQueue/CD5_getMinStack/Main.java
@@ -54,3 +54,36 @@ class MyStack1 {
         return this.stackMin.peek();
     }
 }
+
+class MyStack2 {
+    private Stack<Integer> stackData;
+    private Stack<Integer> stackMin;
+    public MyStack2() {
+        this.stackData = new Stack<>();
+        this.stackMin = new Stack<>();
+    }
+    public void push(int newNum) {
+        if (this.stackMin.isEmpty()) {
+            this.stackMin.push(newNum);
+        } else if (newNum < this.getmin()) {
+            this.stackMin.push(newNum);
+        } else {
+            int newMin = this.stackMin.peek();
+            this.stackMin.push(newMin);
+        }
+        this.stackData.push(newNum);
+    }
+    public int pop() {
+        if (this.stackData.isEmpty()) {
+            throw new RuntimeException("Your stack is empty.");
+        }
+        this.stackMin.pop();
+        return this.stackData.pop();
+    }
+    public int getmin() {
+        if (this.stackMin.isEmpty()) {
+            throw new RuntimeException("Your stack is empty.");
+        }
+        return this.stackMin.peek();
+    }
+}
