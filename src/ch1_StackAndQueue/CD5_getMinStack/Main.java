6ff5cbb0abc3cdf977f1138d4d3fb57c05a37b71
wzh-111 <lics@seu.edu.cn>
2021-05-21 12:09:29 +0800

    min stack

---
+++ src/ch1_StackAndQueue/CD5_getMinStack/Main.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        MyStack1 myStack = new MyStack1();
+        int opNum = Integer.parseInt(in.nextLine());
+        for (int i = 0; i < opNum; i++) {
+            String line = in.nextLine();
+            if ("getMin".equals(line)) {
+                System.out.println(myStack.getmin());
+            } else if ("pop".equals(line)) {
+                myStack.pop();
+            } else {
+                int value = Integer.parseInt(line.split(" ")[1]);
+                myStack.push(value);
+            }
+        }
+    }
+}
+
+
+class MyStack1 {
+    private Stack<Integer> stackData;
+    private Stack<Integer> stackMin;
+    public MyStack1() {
+        this.stackData = new Stack<>();
+        this.stackMin = new Stack<>();
+    }
+    public void push(int newNum) {
+        if (this.stackMin.isEmpty()) {
+            this.stackMin.push(newNum);
+        } else if (newNum <= this.getmin()) {
+            this.stackMin.push(newNum);
+        }
+        this.stackData.push(newNum);
+    }
+    public int pop() {
+        if (this.stackData.isEmpty()) {
+            throw new RuntimeException("Your stack is empty.");
+        }
+        int value = this.stackData.pop();
+        if (value == this.getmin()) {
+            this.stackMin.pop();
+        }
+        return value;
+    }
+    public int getmin() {
+        if (this.stackMin.isEmpty()) {
+            throw new RuntimeException("Your stack is empty.");
+        }
+        return this.stackMin.peek();
+    }
+}
